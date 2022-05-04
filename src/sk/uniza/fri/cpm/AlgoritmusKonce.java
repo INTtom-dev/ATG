d5887475b2df8afed4732649373db3bac4aabe24
Tomáš Šálek <tmssalek@gmail.com>
2022-05-04 12:31:36 +0200

    dokoncena CPM algoritmus

---
+++ src/sk/uniza/fri/cpm/AlgoritmusKonce.java
@@ -0,0 +1,57 @@
+package sk.uniza.fri.cpm;
+
+import sk.uniza.fri.Graf;
+
+import java.util.logging.Logger;
+
+public class AlgoritmusKonce {
+    private Graf graf;
+    private final int pocetVrcholov;
+    private int[] postupnost;
+    private int[] ohodnotenia;
+    private int trvanie;
+    private int[] z;
+
+    public AlgoritmusKonce(Graf graf, int pocetVrcholov, int[] postupnost, int[] ohodnotenia, int trvanie, int[] z) {
+        this.graf = graf;
+        this.pocetVrcholov = pocetVrcholov;
+        this.postupnost = postupnost;
+        this.ohodnotenia = ohodnotenia;
+        this.trvanie = trvanie;
+        this.z = z;
+    }
+
+    /**
+     * Inicializacia k(v) a nasledne pouzity algoritmus
+     */
+    private void urcenieKoncov() {
+        Logger log = Logger.getLogger(AlgoritmusKonce.class.getName());
+        int[] k = new int[pocetVrcholov + 1];
+        for (int i = 1; i <= pocetVrcholov; i++) {
+            k[i] = this.trvanie;
+        }
+        int riadiaciVrchol = 0;
+
+        for (int i = this.pocetVrcholov; i > 0; i--) {
+            riadiaciVrchol = this.postupnost[i];
+            log.info("Vyberám riadiaci vrchol k(" + riadiaciVrchol + ") = " + k[riadiaciVrchol]);
+            for (int j = 0; j < this.graf.getZoznamHran().size(); j++) {
+                if (this.graf.getZoznamHran().get(j).getVrcholZ() == riadiaciVrchol) {
+                    log.info("Vyberám vystupný vrchol riadiaceho vrcholu k(" + this.graf.getZoznamHran().get(j).getVrcholDo() + ") = " + k[this.graf.getZoznamHran().get(j).getVrcholDo()]);
+                    if (k[riadiaciVrchol] > k[this.graf.getZoznamHran().get(j).getVrcholDo()] - this.ohodnotenia[this.graf.getZoznamHran().get(j).getVrcholDo()]) {
+                        k[riadiaciVrchol] = k[this.graf.getZoznamHran().get(j).getVrcholDo()] - this.ohodnotenia[this.graf.getZoznamHran().get(j).getVrcholDo()];
+                        log.info("Nastavujem k(" + riadiaciVrchol + ") = " + k[riadiaciVrchol]);
+                    }
+                }
+            }
+        }
+        log.info("Určené nutné konce pre všetky vrcholy: ");
+        for (int i = 1; i <= pocetVrcholov; i++) {
+            log.info("k(" + i + ") = " + k[i]);
+        }
+        log.info("Časové rezervy pre každý vrchol:");
+        for (int i = 1; i <= pocetVrcholov; i++) {
+            log.info("R(" + i + ") = " + (k[i] - this.ohodnotenia[i]));
+        }
+    }
+}
---
+++ src/sk/uniza/fri/cpm/AlgoritmusZaciatky.java
@@ -0,0 +1,64 @@
+package sk.uniza.fri.cpm;
+
+import sk.uniza.fri.Graf;
+
+import java.util.logging.Logger;
+
+public class AlgoritmusZaciatky {
+    private Graf graf;
+    private final int pocetVrcholov;
+    private int[] postupnosti;
+    private int[] ohodnotenia;
+    private int trvanie;
+    private int[] z;
+
+    public AlgoritmusZaciatky(Graf graf, int pocetVrcholov, int[] postupnosti, int[] ohodnotenia){
+        this.graf = graf;
+        this.pocetVrcholov = pocetVrcholov;
+        this.postupnosti = postupnosti;
+        this.ohodnotenia = ohodnotenia;
+        this.trvanie = 0;
+        this.z = new int[this.pocetVrcholov + 1];
+    }
+    /*
+    * inicializácoa z(v)
+     */
+    public void urciZaciatky() {
+        Logger log = Logger.getLogger(AlgoritmusZaciatky.class.getName());
+
+
+        for (int i = 1; i <= this.pocetVrcholov; i++) {
+            this.z[i] = 0;
+        }
+        int riadiaciVrchol = 0;
+        for (int i = 1; i <= this.pocetVrcholov; i++) {
+            riadiaciVrchol = this.postupnosti[i];
+            log.info("Vyberám riadiaci vrchol z(" + riadiaciVrchol + ") = " +this.z[riadiaciVrchol]);
+            for (int j = 0; j <= this.graf.getZoznamHran().size(); j++) {
+                if (this.graf.getZoznamHran().get(j).getVrcholZ() == riadiaciVrchol) {
+                    log.info("Vyberam výstupný vrchol riadiaceho z(" + this.graf.getZoznamHran().get(j).getVrcholZ() + ") = " + this.z[this.graf.getZoznamHran().get(j).getVrcholDo()]);
+                    if (this.z[this.graf.getZoznamHran().get(j).getVrcholDo()] < this.z[riadiaciVrchol] + this.ohodnotenia[riadiaciVrchol]) {
+                        this.z[this.graf.getZoznamHran().get(j).getVrcholDo()] = this.z[riadiaciVrchol] + this.ohodnotenia[riadiaciVrchol];
+                        Logger.getLogger(AlgoritmusZaciatky.class.getName()).info("Mením z(" + this.graf.getZoznamHran().get(j).getVrcholDo() + ") = " + this.z[this.graf.getZoznamHran().get(j).getVrcholDo()]);
+                    }
+                }
+            }
+        }
+        log.info("Možné začiatkky každého vrcholu: ");
+        for (int i = 1; i <= this.pocetVrcholov; i++) {
+            log.info("z(" + i + ") = " + this.z[i]);
+        }
+        for (int i = 1; i <= this.pocetVrcholov; i++) {
+            if (this.z[i] + this.ohodnotenia[i] > this.trvanie) {
+                this.trvanie = this.z[i] + this.ohodnotenia[i];
+            }
+        }
+        log.info("Trvanie algoritmu: " + this.trvanie);
+    }
+    public int getTrvanie() {
+        return this.trvanie;
+    }
+    public int[] getZ() {
+        return this.z;
+    }
+}
